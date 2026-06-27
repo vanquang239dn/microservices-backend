36f6b63d71a4e74222b3229a5d5679b2d32937fb
unknown <vanquang239dn@gmail.com>
2026-06-27 14:25:26 +0900

    fix bug

--- patient-management/patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-management/patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -3,7 +3,9 @@ package com.pm.patientservice.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pm.patientservice.common.PatientServiceBaseController;
 import com.pm.patientservice.dto.request.PatientRequest;
+import com.pm.patientservice.dto.response.ApiResponse;
 import com.pm.patientservice.dto.response.PatientResponse;
 import com.pm.patientservice.service.PatientService;
 
@@ -16,7 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,48 +29,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/patient")
 @Slf4j(topic = "PATIENT-CONTROLLER")
 @Tag(name = "Patient Controller", description = "API for managing patient")
-public class PatientController {
+public class PatientController extends PatientServiceBaseController {
 
     private final PatientService patientService;
 
     // API for fetching patient list
     @Operation(summary = "Get patient list", description = "Returns a list of patient")
     @GetMapping("/list")
-    public ResponseEntity<List<PatientResponse>> getPatient() {
+    public ApiResponse<List<PatientResponse>> getPatient() {
 
         List<PatientResponse> patients = patientService.getPatients();
 
-        return ResponseEntity.ok().body(patients);
+        return createSuccessResponse(patients);
     }
 
     // API for creating a new patient
     @Operation(summary = "Create a new patient", description = "Returns a new patient")
     @PostMapping("/create")
-    public ResponseEntity<PatientResponse> createPatient(@Valid @RequestBody PatientRequest req) {
+    public ApiResponse<String> createPatient(@Valid @RequestBody PatientRequest req) {
 
-        PatientResponse patientResponse = patientService.createPatient(req);
+        patientService.createPatient(req);
 
-        return ResponseEntity.ok().body(patientResponse);
+        return createSuccessResponse("User created successfully");
     }
 
-    // API for creating a new patient
+    // API for updating a new patient
     @Operation(summary = "Update an exists patient with patient id", description = "Returns an updated patient")
     @PutMapping("/update/{patientId}")
-    public ResponseEntity<PatientResponse> updatePatient(@PathVariable UUID patientId,
+    public ApiResponse<String> updatePatient(@PathVariable UUID patientId,
             @Valid @RequestBody PatientRequest req) {
 
-        PatientResponse patientResponse = patientService.updatePatient(patientId, req);
+        patientService.updatePatient(patientId, req);
 
-        return ResponseEntity.ok().body(patientResponse);
+        return createSuccessResponse("User updated successfully");
     }
 
-    // API for creating a new patient
+    // API for updating a new patient
     @Operation(summary = "Delete an exists patient with patient id", description = "Returns a deleted patient")
     @PutMapping("/delete/{patientId}")
-    public ResponseEntity<PatientResponse> updatePatient(@PathVariable UUID patientId) {
+    public ApiResponse<String> updatePatient(@PathVariable UUID patientId) {
 
-        PatientResponse patientResponse = patientService.deletePatient(patientId);
+        patientService.deletePatient(patientId);
 
-        return ResponseEntity.ok().body(patientResponse);
+        return createSuccessResponse("User deleted successfully");
     }
 }
--- patient-management/patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ patient-management/patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.pm.patientservice.model.Patient;
 
